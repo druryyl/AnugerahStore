b8f317fd38526917a16d53810fd3e49b468c5086
Drury Yudis Lumenta <drury_yl@yahoo.com>
2019-01-31 13:47:44 +0700

    Create Model BukuHutang

---
+++ AnugerahBackend/Keuangan/Model/BukuHutangModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnugerahBackend.Keuangan.Model
+{
+    public class BukuHutangModel
+    {
+        public string BukuHutangID { get; set; }
+        public string TglBuku { get; set; }
+        public string JamBuku { get; set; }
+        public string UserrID { get; set; }
+        public string PihakKetigaID { get; set; }
+        public string PihakKetigaName { get; set; }
+
+        public decimal NilaiHutang { get; set; }
+        public decimal NilaiSisa { get; set; }
+
+        public string Keterangan { get; set; }
+        public string BukuKasID { get; set; }
+    }
+}
