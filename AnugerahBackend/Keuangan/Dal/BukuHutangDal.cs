7f911a6132d78e82b26fd9f8295cc8e02864148c
Drury Yudis Lumenta <drury_yl@yahoo.com>
2019-01-31 14:02:52 +0700

    Develop BukuHutang; DB, Model, Dal, UnitTest Dal

---
+++ AnugerahBackend/Keuangan/Dal/BukuHutangDal.cs
@@ -0,0 +1,204 @@
+﻿using AnugerahBackend.Keuangan.Model;
+using Ics.Helper.Extensions;
+using Ics.Helper.StringDateTime;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnugerahBackend.Keuangan.Dal
+{
+    public interface IBukuHutangDal
+    {
+        void Insert(BukuHutangModel bukuHutang);
+        void Update(BukuHutangModel bukuHutang);
+        void Delete(string bukuHutangID);
+        BukuHutangModel GetData(string bukuHutangID);
+        IEnumerable<BukuHutangModel> ListData(string tgl1, string tgl2);
+
+    }
+    public class BukuHutangDal : IBukuHutangDal
+    {
+        private string _connString;
+
+        public BukuHutangDal()
+        {
+            _connString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+        public void Insert(BukuHutangModel bukuHutang)
+        {
+            var sSql = @"
+                INSERT INTO
+                    BukuHutang (
+                        BukuHutangID, TglBuku, JamBuku, UserrID,
+                        PihakKetigaID, NilaiHutang, NilaiSisa,
+                        Keterangan, BukuKasID)
+                VALUES (
+                        @BukuHutangID, @TglBuku, @JamBuku, @UserrID,
+                        @PihakKetigaID, @NilaiHutang, @NilaiSisa,
+                        @Keterangan, @BukuKasID) ";
+            using (var conn = new SqlConnection(_connString))
+            using (var cmd = new SqlCommand(sSql, conn))
+            {
+                cmd.AddParam("@BukuHutangID", bukuHutang.BukuHutangID);
+                cmd.AddParam("@TglBuku", bukuHutang.TglBuku.ToTglYMD());
+                cmd.AddParam("@JamBuku", bukuHutang.JamBuku);
+                cmd.AddParam("@UserrID", bukuHutang.UserrID);
+                cmd.AddParam("@PihakKetigaID", bukuHutang.PihakKetigaID);
+                cmd.AddParam("@NilaiHutang", bukuHutang.NilaiHutang);
+                cmd.AddParam("@NilaiSisa", bukuHutang.NilaiSisa);
+                cmd.AddParam("@Keterangan", bukuHutang.Keterangan);
+                cmd.AddParam("@BukuKasID", bukuHutang.BukuKasID);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Update(BukuHutangModel bukuHutang)
+        {
+            var sSql = @"
+                UPDATE
+                    BukuHutang 
+                SET 
+                    TglBuku = @TglBuku, 
+                    JamBuku = @JamBuku, 
+                    UserrID = @UserrID,
+                    PihakKetigaID = @PihakKetigaID, 
+                    NilaiHutang = @NilaiHutang, 
+                    NilaiSisa = @NilaiSisa,
+                    Keterangan = @Keterangan, 
+                    BukuKasID = @BukuKasID
+                WHERE
+                    BukuHutangID = @BukuHutangID ";
+            using (var conn = new SqlConnection(_connString))
+            using (var cmd = new SqlCommand(sSql, conn))
+            {
+                cmd.AddParam("@BukuHutangID", bukuHutang.BukuHutangID);
+                cmd.AddParam("@TglBuku", bukuHutang.TglBuku.ToTglYMD());
+                cmd.AddParam("@JamBuku", bukuHutang.JamBuku);
+                cmd.AddParam("@UserrID", bukuHutang.UserrID);
+                cmd.AddParam("@PihakKetigaID", bukuHutang.PihakKetigaID);
+                cmd.AddParam("@NilaiHutang", bukuHutang.NilaiHutang);
+                cmd.AddParam("@NilaiSisa", bukuHutang.NilaiSisa);
+                cmd.AddParam("@Keterangan", bukuHutang.Keterangan);
+                cmd.AddParam("@BukuKasID", bukuHutang.BukuKasID);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(string bukuHutangID)
+        {
+            var sSql = @"
+                DELETE
+                    BukuHutang
+                WHERE
+                    BukuHutangID = @BukuHutangID ";
+            using (var conn = new SqlConnection(_connString))
+            using (var cmd = new SqlCommand(sSql, conn))
+            {
+                cmd.AddParam("@BukuHutangID", bukuHutangID);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public BukuHutangModel GetData(string bukuHutangID)
+        {
+            BukuHutangModel result = null;
+            var sSql = @"
+                SELECT
+                    aa.BukuHutangID, aa.TglBuku, aa.JamBuku, aa.UserrID,
+                    aa.PihakKetigaID, aa.NilaiHutang, aa.NilaiSisa,
+                    aa.Keterangan, aa.BukuKasID,
+                    ISNULL(bb.PihakKetigaName, '') PihakKetigaName
+                FROM    
+                    BukuHutang aa
+                    LEFT JOIN PihakKetiga bb ON aa.PihakKetigaID = bb.PihakKetigaID
+                WHERE
+                    aa.BukuHutangID = @BukuHutangID ";
+            using (var conn = new SqlConnection(_connString))
+            using (var cmd = new SqlCommand(sSql, conn))
+            {
+                cmd.AddParam("@BukuHutangID", bukuHutangID);
+                conn.Open();
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    if (!dr.HasRows) return null;
+                    dr.Read();
+                    result = new BukuHutangModel
+                    {
+                        BukuHutangID = bukuHutangID,
+                        TglBuku = dr["TglBuku"].ToString().ToTglDMY(),
+                        JamBuku = dr["JamBuku"].ToString(),
+                        UserrID = dr["USerrID"].ToString(),
+
+                        PihakKetigaID = dr["PihakKetigaID"].ToString(),
+                        PihakKetigaName = dr["PihakKetigaName"].ToString(),
+
+                        NilaiHutang = Convert.ToDecimal(dr["NilaiHutang"]),
+                        NilaiSisa = Convert.ToDecimal(dr["NilaiSisa"]),
+                        Keterangan = dr["Keterangan"].ToString(),
+                        BukuKasID = dr["BukuKasID"].ToString()
+                    };
+                }
+            }
+            return result;
+        }
+
+        public IEnumerable<BukuHutangModel> ListData(string tgl1, string tgl2)
+        {
+            List<BukuHutangModel> result = null;
+            var sSql = @"
+                SELECT
+                    aa.BukuHutangID, aa.TglBuku, aa.JamBuku, aa.UserrID,
+                    aa.PihakKetigaID, aa.NilaiHutang, aa.NilaiSisa,
+                    aa.Keterangan, aa.BukuKasID,
+                    ISNULL(bb.PihakKetigaName, '') PihakKetigaName
+                FROM    
+                    BukuHutang aa
+                    LEFT JOIN PihakKetiga bb ON aa.PihakKetigaID = bb.PihakKetigaID
+                WHERE
+                    aa.TglBuku BETWEEN @Tgl1 AND @Tgl2 ";
+
+            using (var conn = new SqlConnection(_connString))
+            using (var cmd = new SqlCommand(sSql, conn))
+            {
+                cmd.AddParam("@Tgl1", tgl1.ToTglYMD());
+                cmd.AddParam("@Tgl2", tgl2.ToTglYMD());
+                conn.Open();
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    if (!dr.HasRows) return null;
+                    result = new List<BukuHutangModel>();
+                    while (dr.Read())
+                    {
+                        var item = new BukuHutangModel
+                        {
+                            BukuHutangID = dr["BukuHutangID"].ToString(),
+                            TglBuku = dr["TglBuku"].ToString().ToTglDMY(),
+                            JamBuku = dr["JamBuku"].ToString(),
+                            UserrID = dr["USerrID"].ToString(),
+
+                            PihakKetigaID = dr["PihakKetigaID"].ToString(),
+                            PihakKetigaName = dr["PihakKetigaName"].ToString(),
+
+                            NilaiHutang = Convert.ToDecimal(dr["NilaiHutang"]),
+                            NilaiSisa = Convert.ToDecimal(dr["NilaiSisa"]),
+                            Keterangan = dr["Keterangan"].ToString(),
+                            BukuKasID = dr["BukuKasID"].ToString()
+                        };
+                        result.Add(item);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ AnugerahUnitTest/Keuangan/BukuHutangDalTest.cs
@@ -0,0 +1,144 @@
+﻿using AnugerahBackend.Keuangan.Dal;
+using AnugerahBackend.Keuangan.Model;
+using FluentAssertions;
+using Ics.Helper.Database;
+using Ics.Helper.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AnugerahUnitTest.Keuangan
+{
+
+    public interface IBukuHutangDalTest
+    {
+        void Insert_ValidData_NoEx();
+        void Update_ValidDat_NoEx();
+        void Delete_ValidData_NoEx();
+        void GetData_ValidData_NoEx();
+        void ListData_ValidData_NoEx();
+    }
+
+    public class BukuHutangDalTest : IBukuHutangDalTest
+    {
+        private IBukuHutangDal _jenisTrsHutangirDal;
+
+        public BukuHutangDalTest()
+        {
+            _jenisTrsHutangirDal = new BukuHutangDal();
+        }
+
+        BukuHutangModel BukuHutangDataFactory()
+        {
+            var result = new BukuHutangModel
+            {
+                BukuHutangID = "A1",
+                TglBuku = "20-01-2019",
+                JamBuku = "11:00:12",
+                UserrID = "B1",
+                PihakKetigaID = "C1",
+                PihakKetigaName = "D1",
+                NilaiHutang = 21000,
+                NilaiSisa = 1000,
+                Keterangan = "E1",
+                BukuKasID = "F1"
+            };
+            return result;
+        }
+
+        [Fact]
+        public void Insert_ValidData_NoEx()
+        {
+            using (var trans = TransHelper.NewScope())
+            {
+                //  arrange
+                var expected = BukuHutangDataFactory();
+
+                //  act
+                _jenisTrsHutangirDal.Insert(expected);
+
+                //  assert
+            }
+        }
+
+        [Fact]
+        public void Update_ValidDat_NoEx()
+        {
+            using (var trans = TransHelper.NewScope())
+            {
+                //  arrang
+                var expected = BukuHutangDataFactory();
+
+                //  act
+                _jenisTrsHutangirDal.Update(expected);
+
+                //  assert
+            }
+        }
+
+        [Fact]
+        public void Delete_ValidData_NoEx()
+        {
+            using (var trans = TransHelper.NewScope())
+            {
+                //  arrange
+                var expected = BukuHutangDataFactory();
+                _jenisTrsHutangirDal.Insert(expected);
+
+                //  act
+                _jenisTrsHutangirDal.Delete("A");
+
+                //  assert
+            }
+        }
+
+        [Fact]
+        public void GetData_ValidData_NoEx()
+        {
+            using (var trans = TransHelper.NewScope())
+            {
+                //  arrange
+                var expected = BukuHutangDataFactory();
+                _jenisTrsHutangirDal.Insert(expected);
+
+                //  act
+                var actual = _jenisTrsHutangirDal.GetData("A1");
+
+                //  assert
+                actual.Should().BeEquivalentTo(expected,
+                    config => config
+                        .Excluding(x => x.PihakKetigaName));
+            }
+        }
+
+        [Fact]
+        public void ListData_ValidData_NoEx()
+        {
+            using (var trans = TransHelper.NewScope())
+            {
+                //  arrange
+                var item1 = BukuHutangDataFactory();
+                _jenisTrsHutangirDal.Insert(item1);
+                var item2 = item1.CloneObject();
+                item2.BukuHutangID = "A2";
+                _jenisTrsHutangirDal.Insert(item2);
+                var expected = new List<BukuHutangModel>
+                {
+                    item1, item2
+                };
+
+                //  act
+                var actual = _jenisTrsHutangirDal.ListData(item1.TglBuku, item1.TglBuku);
+
+                //  assert
+                actual.Should().BeEquivalentTo(expected,
+                    config => config
+                        .Excluding(x => x.PihakKetigaName));
+            }
+        }
+
+    }
+}
