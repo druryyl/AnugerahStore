7e9dd6f8be465842793c73d7edb482cd356483a9
Drury Yudis Lumenta <drury_yl@yahoo.com>
2019-03-08 17:53:13 +0700

    Ubah SQL Server ke versi 2014

---
+++ AnugerahWinform/Support/Presenter/LoginPresenter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnugerahBackend.Support.BL;
+using AnugerahWinform.Support.View;
+
+namespace AnugerahWinform.Support.Presenter
+{
+    public class LoginPresenter
+    {
+        private ILoginView _view;
+        private IUserrBL _userrBL;
+
+        public LoginPresenter(ILoginView view)
+        {
+            _view = view;
+            _userrBL = new UserrBL();
+        }
+        public void Login()
+        {
+            var userr = _userrBL.GetData(_view.UserrID);
+            var isValidLogin = _userrBL.IsValidPassword(userr, _view.Password);
+            //if(isValidLogin)
+                
+        }
+    }
+}
---
+++ AnugerahWinform/Support/View/LoginView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnugerahWinform.Support.View
+{
+    public interface ILoginView
+    {
+        string UserrID { get; set; }
+        string Password { get; set; }
+
+    }
+}
